6ab56d934a514e899bcb9023c72f756765b78b4d
Vasser232 <73202398+vasser2323@users.noreply.github.com>
2024-01-08 13:46:23 +0400

    Update AddCustomerServlet.java

--- src/main/java/com/example/Servlets/AddCustomerServlet.java
+++ src/main/java/com/example/Servlets/AddCustomerServlet.java
@@ -18,26 +18,24 @@ public class AddCustomerServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String idStr = request.getParameter("customer_id");
-        int customerId = -1; // Значение по умолчанию, если ID не указан или указан некорректно
+        int customerId = -1;
 
         try {
-            customerId = Integer.parseInt(idStr); // Попытка преобразования строки в число
+            customerId = Integer.parseInt(idStr); 
         } catch (NumberFormatException e) {
-            // В случае некорректного ввода ID или его отсутствия будет использоваться значение по умолчанию (-1)
-            // Можно добавить обработку ошибки или вывести сообщение об ошибке в логи
+
         }
 
         Customer newCustomer = new Customer();
-        newCustomer.setCustomerId(customerId); // Установка ID для нового клиента
+        newCustomer.setCustomerId(customerId); 
 
-        // Получаем остальные данные о клиенте из запроса и устанавливаем их в новый экземпляр клиента
         newCustomer.setCustomerName(request.getParameter("customer_name"));
         newCustomer.setEmail(request.getParameter("email"));
         newCustomer.setPhoneNumber(request.getParameter("phone_number"));
         newCustomer.setAddress(request.getParameter("address"));
 
-        customerBean.addCustomer(newCustomer); // Добавляем клиента через бин
+        customerBean.addCustomer(newCustomer); 
 
-        response.sendRedirect(request.getContextPath() + "/customers"); // Перенаправляем на страницу с клиентами
+        response.sendRedirect(request.getContextPath() + "/customers"); 
     }
 }
