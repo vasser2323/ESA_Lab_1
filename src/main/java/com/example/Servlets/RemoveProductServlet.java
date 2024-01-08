64b3d5de9bf6f561c2e97ce077709b162f530bd1
Vasser232 <73202398+vasser2323@users.noreply.github.com>
2024-01-08 13:50:01 +0400

    Update RemoveProductServlet.java

--- src/main/java/com/example/Servlets/RemoveProductServlet.java
+++ src/main/java/com/example/Servlets/RemoveProductServlet.java
@@ -1,6 +1,6 @@
 package com.example.Servlets;
 
-import com.example.Beans.ProductBean; // Исправлен импорт на ProductBean
+import com.example.Beans.ProductBean; 
 
 import jakarta.ejb.EJB;
 import jakarta.servlet.RequestDispatcher;
@@ -11,20 +11,20 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/removeProduct") // Изменен URL на /removeProduct
+@WebServlet("/removeProduct") 
 public class RemoveProductServlet extends HttpServlet {
 
     @EJB
-    private ProductBean productBean; // Измененная ссылка на ProductBean
+    private ProductBean productBean; 
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Integer productId = Integer.parseInt(request.getParameter("id"));
-        productBean.removeProduct(productId); // Измененный метод
+        productBean.removeProduct(productId);
 
         request.setAttribute("actionNameInf", "Remove");
         request.setAttribute("actionNamePast", "removed");
-        request.setAttribute("name", "Product"); // Измененное имя
-        request.setAttribute("returnLink", "products"); // Измененный URL для перенаправления
+        request.setAttribute("name", "Product"); 
+        request.setAttribute("returnLink", "products"); 
 
         RequestDispatcher dispatcher = request.getRequestDispatcher("/actionSuccess.jsp");
         dispatcher.forward(request, response);
