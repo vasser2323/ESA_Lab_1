fc8152a745e35872043cfd1db03bf8fa7c83ddfc
Vasser232 <73202398+vasser2323@users.noreply.github.com>
2024-01-08 13:43:27 +0400

    Update PaymentMethodBean.java

--- src/main/java/com/example/Beans/PaymentMethodBean.java
+++ src/main/java/com/example/Beans/PaymentMethodBean.java
@@ -6,34 +6,34 @@ import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 
-import com.example.modules.PaymentMethod; // Changed import to PaymentMethod entity
+import com.example.modules.PaymentMethod; 
 
 @Stateless
-public class PaymentMethodBean { // Renamed class to PaymentMethodBean
+public class PaymentMethodBean { 
 
     @PersistenceContext
     private EntityManager entityManager;
 
-    public void addPaymentMethod(PaymentMethod paymentMethod) { // Renamed method to addPaymentMethod
+    public void addPaymentMethod(PaymentMethod paymentMethod) { 
         entityManager.persist(paymentMethod);
     }
 
-    public void removePaymentMethod(Integer methodId) { // Renamed method to removePaymentMethod
+    public void removePaymentMethod(Integer methodId) {
         PaymentMethod paymentMethod = entityManager.find(PaymentMethod.class, methodId);
         if (paymentMethod != null) {
             entityManager.remove(paymentMethod);
         }
     }
 
-    public void updatePaymentMethod(PaymentMethod paymentMethod) { // Renamed method to updatePaymentMethod
+    public void updatePaymentMethod(PaymentMethod paymentMethod) {
         entityManager.merge(paymentMethod);
     }
 
-    public PaymentMethod findPaymentMethodById(Integer methodId) { // Renamed method to findPaymentMethodById
+    public PaymentMethod findPaymentMethodById(Integer methodId) { 
         return entityManager.find(PaymentMethod.class, methodId);
     }
 
-    public List<PaymentMethod> getAllPaymentMethods() { // Renamed method to getAllPaymentMethods
+    public List<PaymentMethod> getAllPaymentMethods() { 
         return entityManager.createQuery("SELECT p FROM PaymentMethod p", PaymentMethod.class).getResultList();
     }
 }
